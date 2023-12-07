85a7d92d60e424712cfe1442b36feaff8c180bc5
LimaFrancisco <francisco1111.fl@protonmail.com>
2023-12-07 11:07:35 -0300

    salary

---
+++ beginner/URI_1007.py
@@ -0,0 +1,7 @@
+employee_number = int(input())
+worked_hour = int(input())
+valor_hour = float(input())
+
+salary = worked_hour * valor_hour
+
+print(f"NUMBER = {employee_number}\nSALARY = U$ {salary:.2f}")
